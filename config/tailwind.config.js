bb8a175600fd87ebb3023ba0b73c64d5188f42d1
おかりな_69B <lcttt.pc@gmail.com>
2025-09-13 23:36:45 +0900

    daisyUIを使えるように

---
+++ config/tailwind.config.js
@@ -0,0 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+};
