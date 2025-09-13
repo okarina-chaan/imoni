42c6cf2cd5bdb129053341fd1c9d8c692e30a383
おかりな_69B <lcttt.pc@gmail.com>
2025-09-13 22:57:36 +0900

    controller作成

--- app/controllers/cooking_controller.rb
+++ app/controllers/cooking_controller.rb
@@ -1,4 +1,30 @@
 class CookingController < ApplicationController
+  def choose_meat
+    @meats = ["豚肉", "牛肉", "鶏肉"]
+  end
+
+  def save_meat
+    session[:meat] = params[:meat]
+    redirect_to choose_vegetables_path
+  end
+
+  def choose_vegetables
+    @vegetables = ["きのこ", "長ねぎ", "里芋", "じゃがいも", "にんじん", "こんにゃく"]
+  end
+
+  def save_vegetables
+    session[:vegetables] = params[:vegetables] || []
+    redirect_to choose_seasoning_path
+  end
+
+  def choose_seasoning
+    @seasonings = ["味噌", "醤油", "コンソメスープ"]
+  end
+
+  def save_seasoning
+    session[:seasoning] = params[:seasoning]
+    redirect_to result_path
+  end
   def result
     meat       = session[:meat]
     vegetables = session[:vegetables] || []
