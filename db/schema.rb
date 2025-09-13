30bbb0aa69a1655a5e9c91cc32bfca3057d109a9
おかりな_69B <lcttt.pc@gmail.com>
2025-09-13 22:30:55 +0900

    migrationファイルを追加: ingredients

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_13_063549) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_13_063800) do
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "category"
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.integer "ingredient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "recipes", force: :cascade do |t|
