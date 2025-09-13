e52858baae8518831d03b135571d2a32f96bda5c
おかりな_69B <lcttt.pc@gmail.com>
2025-09-13 15:50:11 +0900

    初期データを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,25 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+pork = Ingredient.create!(name: "豚肉", category: "肉")
+beef = Ingredient.create!(name: "牛肉", category: "肉")
+chiken = Ingredient.create!(name: "鶏肉", category: "肉")
+
+mashroom = Ingredient.create!(name: "きのこ", category: "野菜")
+other_vegitables = [
+satoimo = Ingredient.create!(name: "里芋", category: "野菜")
+poteto = Ingredient.create!(name: "じゃがいも", category: "野菜")
+carrot = Inredient.create!(name: "にんじん", category: "野菜")
+konnyaku = Ingredient.create!(name: "こんにゃく", category: "野菜")
+]
+naganegi = Ingredient.create!(name: "長ねぎ", category: "野菜")
+
+miso = Ingredient.create!(name: "味噌", category: "調味料")
+syouyu = Ingredient.create!(name: "醤油", category: "調味料")
+chiken_soup = Ingredient.create!(name: "コンソメスープ", category: "調味料")
+
+miyagi    = Recipe.create!(name: "宮城風")
+fukushima = Recipe.create!(name: "福島風")
+yamagata  = Recipe.create!(name: "山形風")
+
+fukushima.ingredients << [pork, mushroom, miso]
+miyagi.ingredients << [pork, *other_vegetables, miso]
+yamagata.ingredients << [beef, green_onion, soy]
+
